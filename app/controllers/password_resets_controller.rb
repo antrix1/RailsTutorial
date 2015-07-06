4d8efc520bfb6ab79a4616c683e75548ce62f6f0
Andrej Dragojevic <andrejdr.93@gmail.com>
2015-07-07 01:04:17 +0200

    valid_user fixing

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -49,7 +49,8 @@ class PasswordResetsController < ApplicationController
 
     # Confirms a valid user.
     def valid_user
-      if !(@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+      unless (@user && @user.activated? &&
+        @user.authenticated?(:reset, params[:id]))
         redirect_to root_url
       end
     end
